926860a1618175f651c1dcd5855af2d71646d3d7
amandaalmeida89 <amandaalmeida89@gmail.com>
2024-01-01 13:39:54 +0100

    test: installed and added jest setup, mocks, and simple tests to render pages with different contents.

---
+++ __tests__/index.test.js
@@ -0,0 +1,75 @@
+import '@testing-library/jest-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import Page from '../src/pages/index'
+import { MockedProvider } from '@apollo/client/testing';
+import GetCurrencyQuery from '../src/gql/getCurrency';
+
+window.scrollTo = jest.fn();
+
+const mocks = [
+  {
+    request: {
+      query: GetCurrencyQuery,
+      variables: {
+        countryName: 'Sweden%'
+      }
+    },
+    result: {
+      data: {
+        item: [
+          {
+            nameEn: "Sweden",
+            currency: [
+              {
+                object: {
+                  nameEn: "Swedish krona"
+                }
+              }
+            ]
+          },
+        ]
+      }
+    }
+  }
+];
+
+describe('Page', () => {
+  it('renders initial page', async () => {
+    const { container } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Page />
+      </MockedProvider>
+    );
+    expect(await screen.findByText('Currency Search')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should return input value and currency selected', async () => {
+    const { container } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Page />
+      </MockedProvider>
+    );
+
+    const input = await screen.getByRole('textbox');
+
+    //focus event without value
+    await fireEvent.focus(input, { target: {}})
+
+    //blur event without value
+    await fireEvent.blur(input, { target: {}})
+
+    //input event value
+    await fireEvent.input(input, { target: { value: 'Sweden' }})
+
+    //focus event with value
+    await fireEvent.focus(input, { target: {}})
+
+    //click event in currency collapse list
+    const list = await screen.findByText('Swedish krona')
+    await fireEvent.click(list)
+
+    expect(input).toHaveValue('Sweden')
+    expect(container).toMatchSnapshot();
+  });
+});
